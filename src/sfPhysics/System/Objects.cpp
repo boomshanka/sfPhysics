bdc081d72ab6df8fc79e92de38be95b7aa315906
boomshanka <satan@underworld.devils-at-underworld>
2011-03-14 23:09:17 +0100

    Added copyright information to the source

--- src/sfPhysics/System/Objects.cpp
+++ src/sfPhysics/System/Objects.cpp
@@ -1,5 +1,23 @@
-#include <sfPhysics/System/Objects.hpp>
+/******************************************************************************
+ **	sfPhysics - A simple 2D physics engine
+ ** Copyright (C) 2011  Olaf Vogt aka Boomshanka
+ ** 
+ ** This program is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU General Public License as published by the Free
+ ** Software Foundation; either version 3 of the License, or (at your option)
+ ** any later version. 
+ ** 
+ ** This program is distributed in the hope that it will be useful, but WITHOUT
+ ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ ** FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ ** more details. 
+ ** 
+ ** You should have received a copy of the GNU General Public License along
+ ** with this program; if not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
+
+#include <sfPhysics/System/Objects.hpp>
 
 
 
@@ -175,12 +193,9 @@ sfp::Vector2f sfp::Object::GetMovement(const sfp::Vector2f& position) const
 
 
 
-
-//------------------------------------------------------------------------------------------//
-									/*SFML_Graphics*/
-//------------------------------------------------------------------------------------------//
-
-
+/******************************************************************************
+ **                              SFML_Graphics                               **
+ ******************************************************************************/
 
 
 #ifdef SFML_ENABLED
@@ -276,10 +291,9 @@ myLengthfactor(lengthfactor)
 }
 
 
-
-
-// ------------------------- End of constructor ------------------------- //
-
+/******************************************************************************
+ **                           End of constructor                             **
+ ******************************************************************************/
 
 
 
